817df4343f92fb6663352bfad9fc78700fef6604
AkaAugustoNeves <augusto.nevesti@gmail.com>
2023-01-09 01:11:15 -0300

    [BACKEND] Project in spring

---
+++ ms-lets/src/main/java/br/com/pilares/mslets/MsLetsApplication.java
@@ -0,0 +1,13 @@
+package br.com.pilares.mslets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLetsApplication.class, args);
+	}
+
+}
